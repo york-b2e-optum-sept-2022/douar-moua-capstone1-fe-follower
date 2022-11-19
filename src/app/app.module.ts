106faecdae0ce6010ddc67f44ac82310c7e7275c
douar <mouadouarzong@gmail.com>
2022-11-19 12:45:49 -0600

    ng g c question-list ng g c question

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { SurveyComponent } from './components/survey/survey.component';
 import { SurveyListComponent } from './components/survey-list/survey-list.component';
+import { QuestionListComponent } from './question-list/question-list.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SurveyComponent,
-    SurveyListComponent
+    SurveyListComponent,
+    QuestionListComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
