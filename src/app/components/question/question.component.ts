6346566ad86390c57dfb1ca1fa5c1db633088666
douar <mouadouarzong@gmail.com>
2022-11-29 16:19:06 -0600

    Capture question response in local file

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -3,6 +3,7 @@ import {IQuestion} from "../../_interfaces/IQuestion";
 import {Subscription} from "rxjs";
 import {QuestionService} from "../../services/question.service";
 import {ISurvey} from "../../_interfaces/ISurvey";
+import {IResponse} from "../../_interfaces/IResponse";
 
 @Component({
   selector: 'app-question',
@@ -30,6 +31,10 @@ export class QuestionComponent implements OnInit, OnDestroy {
   choiceTrue: string = "true"
   choiceFalse: string = "false"
 
+  instance: number = 0
+  responseList: IResponse[] = []
+
+
   constructor(private questionService: QuestionService) {
     this.questionListSub = this.questionService.$questionList.subscribe(
       questionList => this.questionList = questionList);
@@ -45,11 +50,34 @@ export class QuestionComponent implements OnInit, OnDestroy {
     this.question = this.questionList[this.questionIndex];
     this.questionIndex++;
     this.beginSurvey=true
+    this.instance = new Date().getTime()
   }
 
   nextQuestionClick(){
+    let response: IResponse = {
+      answer: <string>this.question.answer,
+      question: this.question.prompt,
+      questionId: <number>this.question.id,
+      surveyId: this.survey.id,
+      surveyTitle: this.survey.title,
+      instance: this.instance
+    }
+
+    // require answer
+    if (response.answer === null || ""){
+      alert("Please answer question before going to the next question!")
+      return
+    }
+
+    // store response in responseList
+    this.responseList.push(response)
+    console.log(this.responseList)
+
+    // show next question & set up next question for show
     this.question = this.questionList[this.questionIndex];
     this.questionIndex++;
+
+    // notify completion of survey
     if (this.questionIndex > this.questionList.length){
       this.completedSurvey = true
     }
